ceda9f09a87396757ca0f35cd05cc6f31e980781
Łukasz Pośpiech <63307235+amWilq@users.noreply.github.com>
2021-06-24 11:23:10 +0200

    sync

--- main.cpp
+++ main.cpp
@@ -45,6 +45,35 @@ void WindowInit()
 }
 
 
+
+class a
+{
+public:
+    int s;
+    void SetS() {
+        cin >> s;
+        cout << s << endl;}
+    int GetS() const { return s; }
+
+
+};
+class A {
+public:
+    int num1,num2;
+    void start()
+    {
+        system("cls");
+        gotoxy(5,15);
+        TextColor(12);
+
+        cout << "Podaj rozmiar planszy, najpierw szerokosci potem wysokośc";
+        cin >> num1 >> num2;
+
+
+    }
+};
+
+
 class cPlayer
 {
 public:
@@ -56,19 +85,33 @@ class cLane
 private:
     deque<bool> cars;
     bool right;
+
 public:
+
+
+
     cLane(int width)
     {
+
+        a a1;
+        a1.SetS();
+        int s1=a1.GetS();
+
         for (int i = 0; i < width; i++)
-            cars.push_back(false); ///Dodaje elemend na koniec. Kierunek elementów, w tym przypadku od lewej strony
+            cars.push_front(rand() % s1 == 2); ///Dodaje elemend na koniec. Kierunek elementów, w tym przypadku od lewej strony
         right = rand() % 2; /// Generuje randomowo false albo true '01'
 
     }
-    void Move()
+
+
+
+    void Ruch()
+
     {
+
         if (right) /// Jeżeli prawda to cała linia przesuwa się od prawej do lewej
         {
-            if (rand() % 10 == 1)
+            if (rand() % 10 == 2)
                 cars.push_front(true);///Przesuwa element na początek
             else
                 cars.push_front(false);///Przesuwa element na początek
@@ -76,7 +119,7 @@ public:
         }
         else ///Jeżeli fałsz to cała linia przesuwa się od prawej do lewej
         {
-            if (rand() % 10 == 1)
+            if (rand() % 10 == 2)
                 cars.push_back(true);///Przesuwa element na koniec
             else
                 cars.push_back(false);///Przesuwa element na koniec
@@ -84,9 +127,13 @@ public:
         }
 
     }
-    bool CheckPos(int pos) { return cars[pos]; }
-    void ChangeDirection() { right = !right; }
+    bool SprawdzeniePozycji(int pos) { return cars[pos]; }
+    void ZmianaKierunku() { right = !right; }
+
+
+
 };
+
 class cGame
 {
 private:
@@ -95,12 +142,14 @@ private:
     int Szerokosc;
     int Punkty;
     cPlayer * Gracz;
+
     vector<cLane*> KierunekPrzeszkod;
 
 public:
     cGame(int w, int h)
 
     /// deklaracja wykosoci i szerowkosci mapy
+
     {
 
         IloscLini = h;
@@ -120,6 +169,65 @@ public:
             delete current;
         }
     }
+    public:
+    int num1;
+    void start()
+    {
+            TextColor(14);
+
+            cout << " \n";
+            cout << "Plansza nr 1: Mała\n";
+            cout << "Plansza nr 2: Średnia\n";
+            cout << "Plansza nr 3: Duża\n";
+            cout << "Plansza nr 4: Długa\n";
+
+            cin >> num1;
+
+
+        switch (num1) {
+
+            case 1:
+                {
+                cGame game (50,4);
+                game.Run();
+                break;
+                }
+            case 2:
+            {
+                cGame game (80,5);
+                game.Run();
+                break;
+            }
+            case 3:
+            {
+                cGame game (80,10);
+                game.Run();
+                break;
+            }
+            case 4:
+            {
+                cGame game (80,15);
+                game.Run();
+                break;
+            }
+            default:
+                system("cls");
+                gotoxy(5,15);
+                TextColor(12);
+                cout << "Zle wybrales, sporbuj jeszcze raz";
+                Sleep(5000);
+                exit(0);
+        }
+
+
+        system("cls");
+        gotoxy(5,15);
+        TextColor(12);
+
+
+
+
+    }
 
 
     void ZdobytePunkty()
@@ -128,7 +236,7 @@ public:
         TextColor(14);
         gotoxy(Szerokosc / 2 - 8, IloscLini / 2);
         cout << "Zdobyles 100 punktow \a "; /// /a wydaje dzwięk po zdobyciu punktów
-        Sleep(1000);
+        Sleep(2000);
         TextColor(7);
     }
 
@@ -143,7 +251,7 @@ public:
             {
                 if (i == 0 && (j == 0 || j == Szerokosc - 1)) cout << "P"; ///Jezeli i nie jest pierwsza linia oraz Linie nie jes trowan ilosci lini(ostatnia Linia)
                 if (i == IloscLini - 1 && (j == 0 || j == Szerokosc - 1)) cout << "K";
-                if (KierunekPrzeszkod[i]->CheckPos(j) && i != 0 && i != IloscLini - 1)
+                if (KierunekPrzeszkod[i]->SprawdzeniePozycji(j) && i != 0 && i != IloscLini - 1)
 
                     cout << "~";
 
@@ -161,7 +269,7 @@ public:
 
         TextColor(15);
         gotoxy(0  , IloscLini+3 );
-        cout << "C++ Crossy Road - autor: amWilq";
+        cout << "C++ Street Cross - autor: Lukasz Pospiech, Grzegorz Suchoń ";
         TextColor(0);
         TextColor(10);
         gotoxy((Szerokosc + 5 ), IloscLini/2 );
@@ -195,8 +303,8 @@ public:
         for (int i = 1; i < IloscLini - 1; i++)
         {
             if (rand() % 10 == 1)
-                KierunekPrzeszkod[i]->Move();
-            if (KierunekPrzeszkod[i]->CheckPos(Gracz->x) && Gracz->y == i)
+                KierunekPrzeszkod[i]->Ruch();
+            if (KierunekPrzeszkod[i]->SprawdzeniePozycji(Gracz->x) && Gracz->y == i)
 
                 quit = true;
         }
@@ -204,12 +312,14 @@ public:
         {
             ZdobytePunkty();
             Gracz->y = 0;
-            KierunekPrzeszkod[rand() % IloscLini]->ChangeDirection();
+            KierunekPrzeszkod[rand() % IloscLini]->ZmianaKierunku();
         }
 
     }
+
     void Run()
     {
+
 
         while (!quit)
         {
@@ -220,6 +330,7 @@ public:
         }
 
         gotoxy(Szerokosc / 2 - 4, IloscLini / 2);
+
         TextColor(12);
         cout << "GAME OVER" << endl;
         Beep(300, 500);
@@ -231,7 +342,8 @@ public:
             char current = _getch();
             if (current == 'y')
             {
-                cGame game(30,4);
+
+                cGame game (100,4); /// NIE MAM POMYSŁY JAK TE GOWNO WYSTARTOWAC Z WARTOSCIAMI WPROWADZONYMI NA POCZATKU!!1
                 game.Run();
                 break;
             }
@@ -242,11 +354,19 @@ public:
         }
     }
 };
+
+
+
+
+
 int main()
 {
     srand(time(NULL));
-    cGame game(50,4);
-    game.Run();
+
+    ///A obj;
+    ///obj.start();
+    cGame game(0,0);
+    game.start();
     getchar();
     return 0;
 }
